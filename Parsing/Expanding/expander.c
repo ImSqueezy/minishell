cdcbb8f3725fa4058bcf2ecb24c286c0d1965fb0
ImSqueezy <ouaallaabdelali1@gmail.com>
2025-06-29 15:22:43 +0100

    export preserve_value() is finished

--- Parsing/Expanding/expander.c
+++ Parsing/Expanding/expander.c
@@ -6,7 +6,7 @@
 /*   By: aouaalla <aouaalla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/21 22:01:37 by aouaalla          #+#    #+#             */
-/*   Updated: 2025/06/28 20:36:41 by aouaalla         ###   ########.fr       */
+/*   Updated: 2025/06/29 15:21:24 by aouaalla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -124,28 +124,64 @@ int	error_checker(t_token *export)
 	return (error_occurance);
 }
 
-char	*preserve_value(char *str, int *quoting)
+int	count_dollar(char *s)
 {
-	char	*new;
-	bool	equal_exists;
+	char	*str;
+	char	var;
+	int		count;
 	int		i;
-	int		j;
 
-	new = malloc(ft_strlen(str) + 3);
-	if (!new)
-		return (NULL);
-	(1) && (equal_exists = false, i = 0, j = 0);
+	str = ft_strchr(s, '=');
+	if (!str)
+		return (0);
+	(1) && (i = 0, count = 0, var = 0);
 	while (str[i])
 	{
-		if (str[i] == '=' && !equal_exists)
-			(1) && (equal_exists = true, new[j++] = str[i++], new[j++] = '\'');
-		else
-			new[j++] = str[i++];
+		if (str[i] == '\'' || str[i] == '"')
+			squoting_traffic(str[i], &var);
+		if (str[i] == '$' && !var)
+			count++;
+		i++;
 	}
-	if (equal_exists)
-		new[j++] = '\'';
+	return (count * 2);
+}
+
+void	helpers_setter(bool *value_portion, char *var, char current_char)
+{
+	if (current_char == '=')
+		*value_portion = true;
+	if (current_char == '\'' || current_char == '"')
+		squoting_traffic(current_char, var);
+}
+
+char	*preserve_value(char *str, int *quoting)
+{
+	bool	value_portion;
+	char	*new;
+	char	var;
+	int		i;
+	int		j;
+	
+	new = malloc(ft_strlen(str) + count_dollar(str) + 1);
+	if (!new)
+		return (NULL);
+	(1) && (i = 0, j = 0, var = 0, value_portion = false);
+	while (str[i])
+	{
+		helpers_setter(&value_portion, &var, str[i]);
+		if (str[i] == '=')
+			value_portion = true;
+		if (str[i])
+		if (value_portion && str[i] == '$' && !var)
+		{
+			(1) && (new[j++] = '"', new[j++] = str[i++]);
+			while (ft_isalnum(str[i]) || str[i] == '_')
+				new[j++] = str[i++];
+			new[j++] = '"';
+		}
+		new[j++] = str[i++];
+	}
 	new[j] = '\0';
-	*quoting = -1;
 	return (free(str), new);
 }
 
