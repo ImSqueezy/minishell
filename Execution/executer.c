7b39356d9ed8962242c4d2cd257e9b6afa021560
ImSqueezy <ouaallaabdelali1@gmail.com>
2025-07-04 21:32:49 +0100

    export (WIP)

--- Execution/executer.c
+++ Execution/executer.c
@@ -6,7 +6,7 @@
 /*   By: aouaalla <aouaalla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/02 20:50:12 by aouaalla          #+#    #+#             */
-/*   Updated: 2025/07/04 10:10:00 by aouaalla         ###   ########.fr       */
+/*   Updated: 2025/07/04 21:21:02 by aouaalla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,17 +24,19 @@ int	is_built_in(t_cmd *cmd)
 
 int	execute_builtin(t_gdata *ptr)
 {
-	if (!ft_strcmp(ptr->cmds->cmd, "echo"))
+	if (!ft_strcmp(*ptr->cmds->cmd, "echo"))
 		return (echo(ptr->cmds->cmd));
-	if (!ft_strcmp(ptr->cmds->cmd, ""))
+	if (!ft_strcmp(*ptr->cmds->cmd, "export"))
 		return (export(ptr));
+	if (!ft_strcmp(*ptr->cmds->cmd, "env") && !ptr->cmds->cmd[1])
+		return (env(ptr->env));
 	return (0);
 }
 
 void	execute(t_gdata *ptr)
 {
 	if (is_built_in(ptr->cmds))
-		ptr->exit = execute_builtin(ptr->cmds->cmd);
+		ptr->exit = execute_builtin(ptr);
 }
 
 void	executer(t_gdata	*data)
